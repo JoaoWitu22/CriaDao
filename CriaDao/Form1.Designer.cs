4856f6658cbdac8b2e12797b85400b23ee8659c5
ox654@g1q2.a1p.me <ox654@g1q2.a1p.me>
2022-02-27 00:23:14 -0300

    V1.0

--- CriaDao/Form1.Designer.cs
+++ CriaDao/Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox_cmd = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Update = new System.Windows.Forms.TextBox();
+            this.textBox_Insert = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_table = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBoxColuns
@@ -58,7 +64,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 376);
+            this.button1.Location = new System.Drawing.Point(139, 402);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -68,9 +74,9 @@
             // 
             // textBox_Head
             // 
-            this.textBox_Head.Location = new System.Drawing.Point(12, 421);
+            this.textBox_Head.Location = new System.Drawing.Point(12, 427);
             this.textBox_Head.Name = "textBox_Head";
-            this.textBox_Head.Size = new System.Drawing.Size(866, 23);
+            this.textBox_Head.Size = new System.Drawing.Size(1061, 23);
             this.textBox_Head.TabIndex = 3;
             // 
             // textBox_parameter
@@ -78,13 +84,13 @@
             this.textBox_parameter.Location = new System.Drawing.Point(220, 27);
             this.textBox_parameter.Multiline = true;
             this.textBox_parameter.Name = "textBox_parameter";
-            this.textBox_parameter.Size = new System.Drawing.Size(374, 372);
+            this.textBox_parameter.Size = new System.Drawing.Size(391, 372);
             this.textBox_parameter.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 405);
+            this.label2.Location = new System.Drawing.Point(12, 409);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 15);
             this.label2.TabIndex = 5;
@@ -116,11 +122,67 @@
             this.label4.TabIndex = 8;
             this.label4.Text = "Sqlcommand";
             // 
+            // textBox_Update
+            // 
+            this.textBox_Update.Location = new System.Drawing.Point(617, 27);
+            this.textBox_Update.Multiline = true;
+            this.textBox_Update.Name = "textBox_Update";
+            this.textBox_Update.Size = new System.Drawing.Size(225, 372);
+            this.textBox_Update.TabIndex = 9;
+            // 
+            // textBox_Insert
+            // 
+            this.textBox_Insert.Location = new System.Drawing.Point(848, 27);
+            this.textBox_Insert.Multiline = true;
+            this.textBox_Insert.Name = "textBox_Insert";
+            this.textBox_Insert.Size = new System.Drawing.Size(225, 372);
+            this.textBox_Insert.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(617, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Update";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(849, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Insert";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 379);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Table";
+            // 
+            // textBox_table
+            // 
+            this.textBox_table.Location = new System.Drawing.Point(107, 376);
+            this.textBox_table.Name = "textBox_table";
+            this.textBox_table.Size = new System.Drawing.Size(107, 23);
+            this.textBox_table.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 450);
+            this.ClientSize = new System.Drawing.Size(1083, 453);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_table);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_Insert);
+            this.Controls.Add(this.textBox_Update);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox_cmd);
             this.Controls.Add(this.label3);
@@ -148,5 +210,11 @@
         private Label label3;
         private TextBox textBox_cmd;
         private Label label4;
+        private TextBox textBox_Update;
+        private TextBox textBox_Insert;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox_table;
     }
 }
--- CriaDao/Form1.cs
+++ CriaDao/Form1.cs
@@ -12,14 +12,25 @@ namespace CriaDao
             string cmd = textBox_cmd.Text;
             textBox_Head.Text = "";
             textBox_parameter.Text = "";
-            string[]coluns = textBoxColuns.Text.Split(new Char[] {',', '.', '-', '\n', '\t' });
-            
-                foreach (string col in coluns)
+            string[] coluns = textBoxColuns.Text.Split(new Char[] { ',', '.', '-', '\n', '\t' });
+            string update = "UPDATE " + textBox_table.Text + " SET ";
+            string insert = "INSERT INTO " + textBox_table.Text + " (";
+            string values = "";
+            string colun = "";
+            foreach (string colr in coluns)
             {
-                col.Replace(" ", "");
-                textBox_parameter.Text = textBox_parameter.Text + cmd +".Parameters.AddWithValue(\"@" + col + "\", " + col + ");\n";
+                var col = colr.Replace(" ", "").Replace("[", "").Replace("]", "").Replace("\n", "");
+                textBox_parameter.Text = textBox_parameter.Text + cmd + ".Parameters.AddWithValue(\"@" + col + "\", " + col + ");\n";
                 textBox_Head.Text = textBox_Head.Text + "string " + col + ", ";
+                update += col + " = @" + col + ",\n";
+                values += "@" + col + ", ";
+                colun += col + ", ";
             }
+            update += " WHERE xxxxxxxxxxxx";
+            insert += colun + ") VALUES (" + values + ")";
+            textBox_Update.Text = update;
+            textBox_Insert.Text = insert;
         }
     }
 }
+
